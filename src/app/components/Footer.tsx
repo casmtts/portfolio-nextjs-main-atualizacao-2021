b3333cfb30d5a113a13af7892b000e0ae4d30d30
csm <contatocsmpm@gmail.com>
2025-06-27 23:11:19 -0300

    fix: copyright ano ajjuste

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -11,7 +11,7 @@ export default function Footer() {
 
   return (
     <footer className=' sm:ml-[4rem] 2xl:ml-[4rem] bg-footer mt-10 min-w-320 ' >
-      <h3 className=' text-white text-center font-[bold] pt-5 ' > {t('copyright')} {new Date().getFullYear()}. </h3>
+      <h3 className=' text-white text-center font-[bold] pt-5 ' > {t('copyright')} 2021. </h3>
       <h5 className=' text-white text-center mt-4 mx-auto ' > {t('paymentsAccepted')} </h5>
       <div className=' flex justify-center mt-[-1rem] ' >
         <Image src={btcIcon} alt="btcIconPayment" width={200} className=' mx:auto ' />
