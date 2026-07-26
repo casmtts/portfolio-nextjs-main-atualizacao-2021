654e9b393b58d1b2067b22918dbd66437a3ad414
Calil Mattos <contatocsmpm@gmail.com>
2026-07-26 15:44:43 -0300

    fix(profile): ajusta layout, responsividade e fontes locais

--- src/app/components/Profile.tsx
+++ src/app/components/Profile.tsx
@@ -2,42 +2,39 @@
 
 import 'animate.css';
 import { useTranslations } from 'next-intl';
-import { Irish_Grover } from 'next/font/google';
 import Image from 'next/image';
 import profilePIC from '../../../public/images/Foto_perfil.svg';
-
-const irish = Irish_Grover({
-  subsets: ['latin'],
-  weight: '400'
-})
+import { irishGrover } from '../fonts';
 
 function Profile() {
-
-  // function TRANSLATE
   const t = useTranslations('ProfilePage');
 
-
   return (
-    <div className='flex ml-[4rem] sm:pb-[18rem] lg:pb-[27rem] w-90% min-w-320 '>
-
-      <div>
-        <div className='animate__animated animate__zoomIn'>
-          <Image className=" sm:w-[10rem] sm:mx-auto sm:mt-[8rem] lg:mt-[15rem] lg:mx-[70%] xl:mx-[77%] 2xl:mx-[75%] 2xl:w-[13rem] 3xl:mx-[80%] " src={profilePIC} alt='profilePIC' />
+    <div className="flex ml-[4rem] sm:pb-[18rem] lg:pb-[27rem] w-[90%] min-w-320">
+      <div className="w-full">
+        <div className="animate__animated animate__zoomIn">
+          <Image
+            className="mx-auto w-[10rem] h-auto sm:mt-[8rem] lg:mt-[15rem] lg:ml-[70%] xl:ml-[77%] 2xl:ml-[75%] 2xl:w-[13rem] 3xl:ml-[80%]"
+            src={profilePIC}
+            alt="profilePIC"
+          />
         </div>
-        <div className=" sm:text-center sm:mt-[2rem] lg:text-2xl lg:ml-[-22rem] lg:mt-[-6rem] 2xl:mt-[-9rem] 2xl:ml-[-30rem] xl:text-4xl 2xl:text-4xl 2xl:text-5xl 3xl:ml-[-35rem] 3xl:mt-[-11rem] text-[#53DA00E6] font-bold "> <p className='animate__bounceIn'>CALIL SOUSA MATTOS</p> </div>
-        <div className='animate__animated animate__pulse'>
-          <div className=" sm:text-center lg:ml-[-21.8rem] xl:text-xl 2xl:text-xl 2xl:ml-[-31rem] 3xl:ml-[-36rem] 3xl:text-2xl font-semibold "> {t('position')} </div>
+        <div className="text-center sm:mt-[2rem] lg:ml-[-10rem] lg:text-2xl lg:mt-[-6rem] 2xl:mt-[-9rem] xl:text-4xl 2xl:text-5xl 3xl:ml-[-25rem] 3xl:mt-[-11rem] text-[#53DA00E6] font-bold">
+          <p className="animate__bounceIn">CALIL SOUSA MATTOS</p>
         </div>
-        <div className={irish.className}>
-          <div className=" sm:mt-[2rem] sm:ml-[1rem] sm:mr-[1rem] lg:ml-[7rem] lg:mr-[25rem] 2xl:text-xl 2xl:ml-[10rem] 2xl:mr-[35%] 3xl:ml-[15rem] 3xl:mr-[40%] 3xl:text-2xl text-justify font-bold font-irish text-[#363537]  ">
-            <p className='animate__animated animate__lightSpeedInLeft'>
+        <div className="animate__animated animate__pulse">
+          <div className="text-center lg:ml-[-10rem] xl:text-xl 2xl:text-xl 3xl:ml-[-25rem] 3xl:text-2xl font-semibold">
+            {t('position')}
+          </div>
+        </div>
+        <div className={irishGrover.className}>
+          <div className="profile-description sm:mt-[2rem] sm:ml-[1rem] sm:mr-[1rem] lg:ml-[15rem] lg:max-w-[42rem] xl:max-w-[46rem] 2xl:ml-[10rem] 2xl:max-w-[50rem] 3xl:ml-[30rem] 3xl:max-w-[56rem] 3xl:text-2xl text-justify font-bold text-[#363537] leading-relaxed whitespace-normal break-words hyphens-auto [overflow-wrap:anywhere]">
+            <p className="animate__animated animate__lightSpeedInLeft">
               {t('description')}
             </p>
           </div>
         </div>
       </div>
-
-
     </div>
   )
 }
---
+++ src/app/fonts.ts
@@ -0,0 +1,15 @@
+import localFont from 'next/font/local';
+
+export const irishGrover = localFont({
+  src: './fonts/IrishGrover-Regular.woff2',
+  display: 'swap',
+  weight: '400',
+  variable: '--font-irish-grover',
+});
+
+export const mohaveLight = localFont({
+  src: './fonts/Mohave-Light.woff2',
+  display: 'swap',
+  weight: '300',
+  variable: '--font-mohave-light',
+});
