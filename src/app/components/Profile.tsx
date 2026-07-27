330347b0e8a193f30919bff4ad85be4e4bdf87b5
Calil Mattos <contatocsmpm@gmail.com>
2026-07-27 17:08:31 -0300

    fix(perfil): centraliza layout no desktop e ajusta safari

--- src/app/components/Profile.tsx
+++ src/app/components/Profile.tsx
@@ -10,25 +10,25 @@ function Profile() {
   const t = useTranslations('ProfilePage');
 
   return (
-    <div className='flex ml-[4rem] sm:pb-[18rem] lg:ml-0 lg:justify-center lg:pb-[27rem] w-90% min-w-320 '>
-      <div className='lg:w-fit'>
+    <div className='profile-shell flex ml-[4rem] sm:pb-[18rem] min-w-320 min-[1025px]:w-[calc(100%-4rem)] min-[1025px]:min-h-[calc(100svh-4rem)] min-[1025px]:items-center min-[1025px]:justify-center min-[1025px]:px-[clamp(6rem,16vw,20rem)] min-[1025px]:pb-0'>
+      <div className='profile-content min-[1025px]:flex min-[1025px]:w-full min-[1025px]:max-w-[58rem] min-[1025px]:flex-col min-[1025px]:items-center min-[1025px]:justify-center min-[1025px]:text-center'>
         <div className='animate__animated animate__zoomIn'>
           <Image
-            className=" sm:w-[10rem] sm:mx-auto sm:mt-[8rem] lg:mt-[15rem] lg:mx-[70%] xl:mx-[77%] 2xl:mx-[75%] 2xl:w-[13rem] 3xl:mx-[150%] "
+            className="sm:w-[10rem] sm:mx-auto sm:mt-[8rem] min-[1025px]:mt-0 min-[1025px]:w-[12rem] min-[1025px]:mx-auto"
             src={profilePIC}
             alt='profilePIC'
           />
         </div>
-        <div className=" sm:text-center sm:mt-[2rem] lg:text-2xl lg:ml-[-22rem] lg:mt-[-6rem] 2xl:mt-[-9rem] 2xl:ml-[-30rem] xl:text-4xl 2xl:text-4xl 2xl:text-5xl 3xl:ml-[-35rem] 3xl:mt-[-11rem] text-[#53DA00E6] font-bold ">
+        <div className="profile-name sm:text-center sm:mt-[2rem] min-[1025px]:mt-6 min-[1025px]:text-4xl min-[1025px]:leading-tight text-[#53DA00E6] font-bold">
           <p className='animate__bounceIn'>CALIL SOUSA MATTOS</p>
         </div>
         <div className='animate__animated animate__pulse'>
-          <div className=" sm:text-center lg:ml-[-21.8rem] xl:text-xl 2xl:text-xl 2xl:ml-[-31rem] 3xl:ml-[-36rem] 3xl:text-2xl font-semibold ">
+          <div className="profile-position sm:text-center min-[1025px]:mt-2 min-[1025px]:text-2xl font-semibold">
             {t('position')}
           </div>
         </div>
         <div className={irishGrover.className}>
-          <div className=" sm:mt-[2rem] sm:ml-[1rem] sm:mr-[1rem] lg:mt-[2rem] lg:ml-[-22rem] lg:mr-[25rem] lg:max-w-[52rem] xl:max-w-[58rem] 2xl:mt-[2rem] 2xl:ml-[-30rem] 2xl:mr-[35%] 2xl:max-w-[64rem] 3xl:ml-[-35rem] 3xl:mr-[30%] 3xl:max-w-[70rem] 3xl:text-2xl text-justify font-bold font-irish text-[#363537]  ">
+          <div className="profile-description sm:mt-[2rem] sm:ml-[1rem] sm:mr-[1rem] min-[1025px]:mt-6 min-[1025px]:max-w-[58rem] min-[1025px]:mx-auto min-[1025px]:text-2xl text-justify font-bold font-irish text-[#363537]">
             <p className='animate__animated animate__lightSpeedInLeft'>
               {t('description')}
             </p>
