470cf3a70934a49bee608ae7b29a7e19d7ebcd1a
Calil Mattos <contatocsmpm@gmail.com>
2026-09-21 14:51:52 -0300

    fix: ajusta controles da topbar no mobile

--- src/app/components/DropdownCV.tsx
+++ src/app/components/DropdownCV.tsx
@@ -45,7 +45,7 @@ export default function Example() {
         leaveFrom="transform opacity-100 scale-100"
         leaveTo="transform opacity-0 scale-95"
       >
-        <MenuItems className="absolute z-10 mt-2 w-[8rem] origin-top-right divide-y divide-[var(--line)] rounded-md bg-[var(--surface)] shadow-lg ring-1 ring-black/10 focus:outline-none">
+        <MenuItems className="absolute right-0 z-10 mt-2 w-[8rem] max-w-[calc(100vw-1.5rem)] origin-top-right divide-y divide-[var(--line)] rounded-md bg-[var(--surface)] shadow-lg ring-1 ring-black/10 focus:outline-none">
           <div className="py-1">
             {/* Curriculum */}
             <MenuItem>
